b5ec34fcc051e405297f476c9ec577185a7a8efb
Judson Mitchell <judsonmitchell@gmail.com>
2012-10-27 09:59:06 -0500

    Add template include

--- lib/php/data/home_trends_load.php
+++ lib/php/data/home_trends_load.php
@@ -2,5 +2,12 @@
 session_start();
 require('../auth/session_check.php');
 require('../../../db.php');
-echo "trends here";
 
+//What kind of charts?
+// 1. view all cases - see most active groups, users, cases
+// 2. supervises - see most active users, cases
+// 3. is supervised - see most active users in their group, most active cases
+//All of these are over a user-selected time period
+
+
+include('../../../html/templates/interior/home_trends.php');
