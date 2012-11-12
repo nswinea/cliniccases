de264e25289194e5b84c848741715c5592b2fb09
Judson Mitchell <judsonmitchell@gmail.com>
2012-11-12 06:42:27 -0600

    First changes to Trends branch

--- lib/php/data/home_trends_load.php
+++ lib/php/data/home_trends_load.php
@@ -2,6 +2,9 @@
 session_start();
 require('../auth/session_check.php');
 require('../../../db.php');
+require('../users/user_data.php');
+
+$user = $_SESSION['login'];
 
 //What kind of charts?
 // 1. view all cases - see most active groups, users, cases
@@ -9,5 +12,11 @@ require('../../../db.php');
 // 3. is supervised - see most active users in their group, most active cases
 //All of these are over a user-selected time period
 
+if ($_SESSION['permissions']['supervises'] == '1')
+{
+	$q = all_users_by_supvsr($dbh,$user);
+
+	//print_r($q);
+}
 
 include('../../../html/templates/interior/home_trends.php');
